0adc4fbc3e08eff1b78d770050ecc3b03cf8b291
Wynder <wcr1987@gmail.com>
2025-08-28 15:55:32 -0500

    Cambios

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,14 +1,47 @@
 module.exports = [
   'strapi::errors',
   {
-    name: 'strapi::cors',
+    name: 'strapi::security',
     config: {
-      origin: ['*'], // <-- tu dominio de Netlify
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-      headers: '*',
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'default-src': ["'self'"],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://market-assets.strapi.io',
+            'https://res.cloudinary.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://res.cloudinary.com',
+          ],
+          'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+          'style-src': ["'self'", "'unsafe-inline'"],
+          'connect-src': [
+            "'self'",
+            'https:',
+            'http://localhost:4200',      // 👈 tu Angular local
+            'https://tu-dominio-frontend.com', // 👈 frontend desplegado
+          ],
+        },
+      },
     },
   },
-  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['*'],
+      headers: '*',  // o define explícitos ['Content-Type', 'Authorization', ...]
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
